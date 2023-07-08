26e9ec528e6c53e06bdad9d09c36a4c67e5494b9
Claudia Lam <claudiaslam@gmail.com>
2023-07-07 19:22:42 -0700

    create User model

--- models.py
+++ models.py
@@ -1,1 +1,40 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+    You should call this in your Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """User Model"""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    first_name = db.Column(
+        db.String(50),
+        nullable=False)
+
+    last_name = db.Column(
+        db.String(50),
+        nullable=False)
+
+    image_url = db.Column(
+        db.String(100),
+        nullable=False,
+        default="https://cdn5.vectorstock.com/i/1000x1000/45/79/male-avatar-profile-picture-silhouette-light-vector-4684579.jpg"
+    )
