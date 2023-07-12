abfbde29b7a16920ec385fc4b188e6b0044ce8a9
Claudia Lam <claudiaslam@gmail.com>
2023-07-12 15:54:29 -0700

    refactor get_full_name method to be a property decorator

--- models.py
+++ models.py
@@ -41,8 +41,9 @@ class User(db.Model):
         default=DEFAULT_IMAGE_URL
     )
 
-    def get_full_name(self):
-        """get full name. """
+    @property
+    def full_name(self):
+        """return get full name. """
 
         return f"{self.first_name} {self.last_name}"
 
